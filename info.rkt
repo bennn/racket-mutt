ae5434907fba5aa49a755047575fba3832092ff2
Ben Greenman <benjaminlgreenman@gmail.com>
2016-10-02 15:19:40 -0400

    ** bump version, remove 6.2 from .travis
    Can't support 6.2 because of the freeform strings in the info.rkt

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define deps '("base" "typed-racket-lib" "typed-racket-more"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "rackunit-abbrevs"))
 (define pkg-desc "API for the Mutt email client")
-(define version "0.1")
+(define version "0.2")
 (define pkg-authors '(ben))
 (define scribblings '(("scribblings/mutt.scrbl" () ("Email"))))
 (define pre-install-collection "private/pre-install.rkt")
