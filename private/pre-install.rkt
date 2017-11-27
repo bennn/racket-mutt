edd94e064993f8e079fd445a6742c884fcbfcbc6
Ben Greenman <benjaminlgreenman@gmail.com>
2017-11-26 20:05:30 -0500

    add working gmail SMTP config
    Now when I run the pre-installer, I'm able to send messages from my
     gmail address & receive them without a warning that the message 'might not have
     been sent' by me.

--- private/pre-install.rkt
+++ private/pre-install.rkt
@@ -19,7 +19,6 @@
               (getenv "TRAVIS"))
     (check-system-type)
     (check-mutt)
-    (check-mail)
     (check-muttrc))
   (void))
 
@@ -34,16 +33,6 @@
   (unless (find-executable-path "mutt")
     (raise-user-error errloc "could not locate `mutt` executable, install with your package manager and try again")))
 
-;; Check for a mailbox folder
-;;  and other config folders/files
-(define (check-mail)
-  (define home (find-system-path 'home-dir))
-  (define home/.mail (build-path home ".mail"))
-  (when (and (not (directory-exists? home/.mail))
-             (bool-prompt "Directory ~a does not exist. Create it? [y/n]" home/.mail))
-    (make-directory home/.mail))
-  (void))
-
 ;; Check if `.muttrc` exists,
 ;;  if not, help user create one
 (define (check-muttrc)
@@ -52,45 +41,34 @@
   (define home/.mutt/muttrc (build-path home ".mutt" "muttrc"))
   (unless (or (file-exists? home/.muttrc)
               (file-exists? home/.mutt/muttrc))
-    (printf "could not locate `muttrc` file at '~a' or '~a'\n" home/.muttrc home/.mutt/muttrc)
-    (printf "creating '~a' ...\n" home/.muttrc)
+    (printf "could not locate `muttrc` file at '~a' or '~a'~n" home/.muttrc home/.mutt/muttrc)
+    (printf "creating '~a' ...~n" home/.muttrc)
     (call-with-output-file home/.muttrc
       (lambda (cfg)
         (define email-addr (email-prompt "for the 'from' field of outgoing messages"))
-        ;; -- basic config, credits to http://www.calmar.ws/mutt/
-        (fprintf cfg "unmy_hdr *    # delete existing header settings, if any\n")
-        (fprintf cfg "# my_hdr X-Homepage: http://www.google.com    # Uncomment to add your homepage to the header\n")
-        (fprintf cfg "set from=\"~a\"\n" email-addr)
-        (fprintf cfg "set realname=\"~a\"\n" (string-prompt "Please enter your name:"))
+        (fprintf cfg "set realname=\"~a\"~n" (string-prompt "Please enter your name:"))
+        (fprintf cfg "set from=\"~a\"~n" email-addr)
+        (fprintf cfg "set use_from = yes~n")
         (if (and (string-suffix? email-addr "@gmail.com")
-                 #f ;; TODO figure out how to authenticate with gmail
-                 (bool-prompt "Are you sending from a gmail account?"))
+                 (bool-prompt "Configure gmail SMTP settings?"))
           (let ([gmail-user (car (string-split email-addr "@"))]
                 [gmail-pass (string-prompt "Enter your gmail password (leave blank to fill in later)")])
-            (fprintf cfg "set imap_user = \"~a\"\n" email-addr)
-            (fprintf cfg "set imap_pass = \"~a\"\n" gmail-pass)
-            (fprintf cfg "set imap_keepalive = 900\n")
-            (fprintf cfg "set folder = imaps://imap.gmail.com/\n")
-            (fprintf cfg "set record = \"+[Gmail]/Sent Mail\"\n")
-            (fprintf cfg "set postponed = \"+[Gmail]/Drafts\"\n")
+            (fprintf cfg "set imap_user = \"$from\"~n")
+            (fprintf cfg "set imap_pass = \"~a\"~n" gmail-pass)
+            (fprintf cfg "set smtp_url = \"smtps://$imap_user@smtp.gmail.com:465/\"~n")
+            (fprintf cfg "set smtp_pass = \"$imap_pass\"~n")
+            (fprintf cfg "set folder = \"imaps://imap.gmail.com:993\"~n")
             (void))
           (begin
-            (fprintf cfg "set folder=\"~~/.mail\"\n")
-            (fprintf cfg "set record=\"+Sent-`date +%Y`\"       # sent messages goes there (e.g. $folder/Sent-2006)\n")
-            (fprintf cfg "set postponed=+postponed              # an 'internal' box for mutt basically\n")
+            (fprintf cfg "set folder=\"~~/.mutt/mail\"~n")
             (void)))
-        (fprintf cfg "set spoolfile=+INBOX                  # incoming mails (~~/.mail/inbox)\n")
-        (fprintf cfg "set mbox_type=mbox\n")
-        (fprintf cfg "set move=yes                          # yes (move read mails automatically to $mbox)\n")
-        (fprintf cfg "set keep_flagged=yes                  # esc-f to mark message in spool, and it won't move to $mbox)\n")
-        (fprintf cfg "set mbox=+read_inbox                  # ~~/.mail/read_inbox\n")
-        (fprintf cfg "set header_cache=~~/.mail/mutt_cache/ # a much faster opening of mailboxes\n")
-        (fprintf cfg "set timeout=10                        # mutt 'presses' (like) a key for you (while you're idle) \n")
-        (fprintf cfg "                                      # each x sec to trigger the thing below\n")
-        (fprintf cfg "set mail_check=5                      # mutt checks for new mails on every keystroke\n")
-        (fprintf cfg "                                      # but not more often then once in 5 seconds\n")
-        (fprintf cfg "set beep_new                          # beep on new messages in the mailboxes\n")
-        (fprintf cfg "\n# Mutt Guide: https://dev.mutt.org/trac/wiki/MuttGuide\n")
+        (fprintf cfg "set spoolfile=+INBOX~n")
+        (fprintf cfg "set ssl_force_tls = yes~n")
+        (fprintf cfg "set smtp_authenticators = 'gssapi:login'~n")
+        (fprintf cfg "set header_cache=~~/.mutt/cache/~n")
+        (fprintf cfg "set timeout=10  # seconds~n")
+        (fprintf cfg "set mail_check=5  # seconds~n")
+        (fprintf cfg "~n# Mutt Guide: https://dev.mutt.org/trac/wiki/MuttGuide~n")
         (void)))))
 
 ;; -----------------------------------------------------------------------------
@@ -120,7 +98,7 @@
     v))
 
 (define (bool-prompt msg . arg*)
-  (prompt/fail (apply format msg arg*)
+  (prompt/fail (apply format (string-append msg " [y/n]") arg*)
     (lambda (x)
       (case (string-downcase x)
        [("y" "yes")
