66d33f30ceaf94857257b9624de4e4866665956d
Ben Greenman <benjaminlgreenman@gmail.com>
2018-10-09 20:22:35 -0400

    ** update deps

--- info.rkt
+++ info.rkt
@@ -1,9 +1,9 @@
 #lang info
 (define collection "mutt")
-(define deps '("base" "typed-racket-lib" "typed-racket-more"))
+(define deps '("base" "typed-racket-lib" "typed-racket-more" "make-log-interceptor"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "rackunit-abbrevs" "typed-racket-doc"))
 (define pkg-desc "API for the Mutt email client")
-(define version "0.2")
+(define version "0.3")
 (define pkg-authors '(ben))
 (define scribblings '(("scribblings/mutt.scrbl" () ("Email"))))
 (define pre-install-collection "private/pre-install.rkt")
